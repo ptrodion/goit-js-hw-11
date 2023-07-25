b4257a2bb2f3d9053467755eae451edf9651a8ab
ptrodion <rdnpetrenko@gmail.com>
2023-07-25 23:31:03 +0100

    add 2

--- src/main.js
+++ src/main.js
@@ -39,24 +39,33 @@ async function handlerSubmitSearch(e) {
   }
 }
 
-window.scrollBy({
-  top: 800,
-  behavior: 'smooth',
-});
-
 window.addEventListener('scroll', () => {
   const documentReact = document.documentElement.getBoundingClientRect();
 
   if (documentReact.bottom < document.documentElement.clientHeight + 150) {
-    addGallery((page += 2));
+    addGallery((page += 1));
   }
+});
+
+window.scrollBy({
+  top: 2000,
+  behavior: 'smooth',
 });
 
 async function addGallery(page) {
   try {
     const results = await getList(searchInput, page);
-    console.log(results.hits);
+
     addMarkup(galleryEl, markupElements(results.hits));
+
+    const { height: cardHeight } = document
+      .querySelector('.gallery')
+      .firstElementChild.getBoundingClientRect();
+
+    window.scrollBy({
+      top: cardHeight * 2,
+      behavior: 'smooth',
+    });
   } catch (error) {
     Notiflix.Notify.failure(error.message);
   }
