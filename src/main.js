a2e7215e250b4ebbcf52d63deca22b9e098cb2c6
ptrodion <rdnpetrenko@gmail.com>
2023-07-26 21:17:04 +0100

    add last changes

--- src/main.js
+++ src/main.js
@@ -2,40 +2,52 @@ import Notiflix from 'notiflix';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
-import { searchFormEl, galleryEl } from './refs/refs';
+import { searchFormEl, galleryEl, mask } from './refs/refs';
 import { getList } from './api/api';
 import { markupElements } from './markup/markup';
 import { createMarkup, addMarkup } from './helpers/Markup';
 
 searchFormEl.addEventListener('submit', handlerSubmitSearch);
 
-let page = 1;
 let searchInput = null;
+let page = 1;
 
 const lightbox = new SimpleLightbox('.gallery .photo-card a', {
   captionDelay: 250,
 });
 
+// const mask = document.querySelector('.mask');
+// function showLoader() {
+//   mask.classList.remove('hide');
+//   setTimeout(() => {
+//     mask.classList.add('hide');
+//   }, 60);
+// }
+
 async function handlerSubmitSearch(e) {
   e.preventDefault();
   searchInput = e.target.elements.searchQuery.value;
-
   if (!searchInput.trim()) {
     Notiflix.Notify.info('You need to fill in the input');
     return;
   } else {
-    try {
-      galleryEl.innerHTML = ' ';
-      lightbox.refresh();
+    mask.classList.remove('hide');
+    setTimeout(async () => {
+      try {
+        galleryEl.innerHTML = ' ';
+        lightbox.refresh();
 
-      const results = await getList(searchInput, page);
-      Notiflix.Notify.success(
-        `"Hooray! We found ${results.totalHits} images."`
-      );
-      createMarkup(galleryEl, markupElements(results.hits));
-    } catch (error) {
-      Notiflix.Notify.failure(error.message);
-    }
+        const results = await getList(searchInput, page);
+
+        Notiflix.Notify.success(
+          `"Hooray! We found ${results.totalHits} images."`
+        );
+        createMarkup(galleryEl, markupElements(results.hits));
+      } catch (error) {
+        Notiflix.Notify.failure(error.message);
+      }
+      mask.classList.add('hide');
+    }, 600);
   }
 }
 
@@ -43,29 +55,29 @@ window.addEventListener('scroll', () => {
   const documentReact = document.documentElement.getBoundingClientRect();
 
   if (documentReact.bottom < document.documentElement.clientHeight + 150) {
-    addGallery((page += 1));
-  }
-});
+    mask.classList.remove('hide');
+    setTimeout(() => {
+      addGallery((page += 1));
+      mask.classList.add('hide');
+    }, 1200);
 
-window.scrollBy({
-  top: 2000,
-  behavior: 'smooth',
+    console.log(page);
+  }
 });
 
 async function addGallery(page) {
   try {
     const results = await getList(searchInput, page);
-
     addMarkup(galleryEl, markupElements(results.hits));
 
     const { height: cardHeight } = document
       .querySelector('.gallery')
       .firstElementChild.getBoundingClientRect();
 
-    window.scrollBy({
-      top: cardHeight * 2,
-      behavior: 'smooth',
-    });
+    // window.scrollBy({
+    //   top: cardHeight * 2,
+    //   behavior: 'smooth',
+    // });
   } catch (error) {
     Notiflix.Notify.failure(error.message);
   }
--- src/refs/refs.js
+++ src/refs/refs.js
@@ -1,2 +1,3 @@
 export const searchFormEl = document.querySelector('.search-form');
 export const galleryEl = document.querySelector('.gallery');
+export const mask = document.querySelector('.mask');
